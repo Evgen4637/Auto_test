7cfdd62ab64fb9441480740ae2f230b17709ea1e
Dart <surmin.work@gmail.com>
2025-02-24 10:30:12 +0300

    задание для темы Утверждения

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,9 +1,10 @@
 from capitalize import capitalize
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
 
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
+assert capitalize('hello') == 'Hello'
+#    raise Exception('Функция работает неверно!')
+
+assert capitalize('') == ''
+#    raise Exception('Функция работает неверно!')
 
 print('Все тесты пройдены!')
